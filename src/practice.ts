94dadb50528fe14f5bb38c4a3e783b8081fb7895
Ha0 <hayeong28@naver.com>
2020-07-16 00:08:24 +0900

    learn: ts type basic

--- src/practice.ts
+++ src/practice.ts
@@ -1,2 +1,18 @@
+let count = 0;
+count += 1;
+count = "문자열"; // error 자동으로 타입 할당되어있음
+
 const message: string = "hello world";
-console.log(message);
+const done: boolean = false;
+
+const numbers: number[] = [1, 2, 3];
+const messages: string[] = ["hello", "world"];
+
+message.push(1); // error string[] 타입에 인트 추가 X
+
+let mightBeUndefined: string | undefined = undefined; // Union Type
+let nullableNumber: number | null = 10; // null
+
+let color: "red" | "orange" | "yellow" = "red"; // literal string type (union)
+color = "yellow";
+color = "green"; // error liter string type에  없음
