1dd8d0a31b0ecc7efa72b26a1c96de9c7ee64b95
Ha0 <hayeong28@naver.com>
2020-07-17 04:04:06 +0900

    learn - contextAPI 타입스크립트로 사용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReducerSample from "./ReducerSample";
+import { SampleProvider } from "./SampleContext";
 
 const App: React.FC = () => {
-  return <ReducerSample />;
+  return (
+    <SampleProvider>
+      <ReducerSample />;
+    </SampleProvider>
+  );
 };
 export default App;
---
+++ src/SampleContext.tsx
@@ -0,0 +1,81 @@
+import React, { createContext, Dispatch, useReducer, useContext } from "react";
+
+type Color = "red" | "orange" | "yellow";
+
+type State = {
+  count: number;
+  text: string;
+  color: Color;
+  isGood: boolean;
+};
+
+type Action =
+  | { type: "SET_COUNT"; count: number }
+  | { type: "SET_TEXT"; text: string }
+  | { type: "SET_COLOR"; color: Color }
+  | { type: "TOGGLE_GOOD" };
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "SET_COUNT":
+      return {
+        ...state,
+        count: action.count,
+      };
+    case "SET_TEXT":
+      return {
+        ...state,
+        text: action.text,
+      };
+    case "SET_COLOR":
+      return {
+        ...state,
+        color: action.color,
+      };
+    case "TOGGLE_GOOD":
+      return {
+        ...state,
+        isGood: !state.isGood,
+      };
+    default:
+      throw new Error("UnHandled action type");
+  }
+}
+
+type SampleDispatch = Dispatch<Action>;
+
+const SampleStateContext = createContext<State | null>(null);
+const SampleDispatchContext = createContext<SampleDispatch | null>(null);
+
+type SampleProviderProps = {
+  children: React.ReactNode;
+};
+
+export function SampleProvider({ children }: SampleProviderProps) {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    text: "hello",
+    color: "red",
+    isGood: true,
+  });
+
+  return (
+    <SampleStateContext.Provider value={state}>
+      <SampleDispatchContext.Provider value={dispatch}>
+        {children}
+      </SampleDispatchContext.Provider>
+    </SampleStateContext.Provider>
+  );
+}
+
+export function useSampleState() {
+  const context = useContext(SampleStateContext);
+  if (!context) throw new Error("Cannot find SampleProvider");
+  return context;
+}
+
+export function useSampleDispatch() {
+  const context = useContext(SampleDispatchContext);
+  if (!context) throw new Error("Cannot find SampleProvider");
+  return context;
+}
