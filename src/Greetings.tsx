0e6df8dd286fbed3cf6148c7cad9bf5c3728b70c
Ha0 <hayeong28@naver.com>
2020-07-17 00:45:33 +0900

    add: function 키워드로 구현하면 defaultProps 쓸 수 있다 쓰자

--- src/Greetings.tsx
+++ src/Greetings.tsx
@@ -2,29 +2,19 @@ import React from "react";
 
 type GreetingsProps = {
   name: string;
-  mark?: string; // defaultProps로 쓰려면 ? 해줘야된다.
-  array?: string[];
+  mark: string; // defaultProps로 쓰려면 ? 해줘야된다.
 };
 
-const Greetings: React.FC<GreetingsProps> = ({
-  name,
-  mark,
-  array = ["a", "b", "c"],
-}) => {
-  // 아래와 같이 map 쓸 수 없다. array type이 string|undefined이기 때문
-  // array.map()
-
+function Greetings({ name, mark }: GreetingsProps) {
   return (
     <div>
       Hello,{name} {mark}
     </div>
   );
-};
+}
 
-// default 쓰지 말고 파라미터에 default값으로 주자.
 Greetings.defaultProps = {
   mark: "!",
-  array: ["a", "b", "c"],
 };
 
 export default Greetings;
